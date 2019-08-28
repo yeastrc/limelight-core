910aca1d25dc426684601c9c984b61348f2388d0
Michael Riffle <mriffle@uw.edu>
2019-08-28 16:40:26 -0700

    many updates

--- limelight_webapp/front_end/src/js/page_js/data_pages/project_search_ids_driven_pages/mod_view_page/modViewMainDataVizRender_MultiSearch.js
+++ limelight_webapp/front_end/src/js/page_js/data_pages/project_search_ids_driven_pages/mod_view_page/modViewMainDataVizRender_MultiSearch.js
@@ -14,36 +14,145 @@ export class ModViewDataVizRenderer_MultiSearch {
                               searchDetailsBlockDataMgmtProcessing,
                               dataPageStateManager_DataFrom_Server } ) {
 
+        console.log(projectSearchIds);
+
         const modMap = ModViewDataVizRenderer_MultiSearch.buildModMap({ reportedPeptideModData, aminoAcidModStats, projectSearchIds });
         const modMatrix = ModViewDataVizRenderer_MultiSearch.getModMatrix({modMap, projectSearchIds});
-        const sortedModMasses = Object.keys(modMap).sort();
+        const sortedModMasses = Object.keys(modMap).sort( (a,b) => ( parseInt(a) - parseInt(b)));
         const maxPsmCount = ModViewDataVizRenderer_MultiSearch.getMaxPSMCount(modMatrix);
+
+        console.log(sortedModMasses);
 
         // if an existing viz is here, blow it away
         let $mainContentDiv = $('#mod_list_container');
         $mainContentDiv.empty();
 
-        const margin = {top: 50, right: 400, bottom: 200, left: 100};
-        const width = 1000;
-        const height = 500;
+        // some defaults for the viz
+        const margin = {top: 200, right: 10, bottom: 10, left: 300};
+        const widthDefs = {default:1000, min: 2, max: 40};
+        const heightDefs = {default:500, min:40, max:40};
 
+        // label defs
+        const labelFontSize = 14;               // font size (in pixels) of labels
+        const maxSearchLabelLength = 43;        // max # of characters in a search label before truncation
+
+        const width = ModViewDataVizRenderer_MultiSearch.getWidth({sortedModMasses, widthDefs});
+        const height = ModViewDataVizRenderer_MultiSearch.getHeight({projectSearchIds, heightDefs});
+
+        // set up our scales
         let xScale = d3.scaleBand()
-            .domain(Object.keys(modMap).sort())
+            .domain(sortedModMasses)
             .range([0,width]);
 
         let yScale = d3.scaleBand()
-            .domain(projectSearchIds.sort())
-            .range([height, 0]);
+            .domain(projectSearchIds)
+            .range([0, height]);
 
         let colorScale = d3.scaleSqrt()
             .domain([0, maxPsmCount])
-            .range([d3.rgb(255,255,255), d3.rgb(255,0,0)])
+            .range([d3.rgb(255,255,255), d3.rgb(255,0,0)]);
 
+        // start drawing the actual viz
         let svg = d3.select("#mod_list_container").append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        ModViewDataVizRenderer_MultiSearch.addColoredRectangles({ svg, modMatrix, xScale, yScale, colorScale, sortedModMasses, projectSearchIds });
+        ModViewDataVizRenderer_MultiSearch.addSeparatorLines({ svg, projectSearchIds, yScale, width, height });
+        ModViewDataVizRenderer_MultiSearch.addSearchLabels({ svg, projectSearchIds, yScale, searchDetailsBlockDataMgmtProcessing, maxSearchLabelLength, labelFontSize });
+        ModViewDataVizRenderer_MultiSearch.addModLabels({ svg, sortedModMasses, xScale, labelFontSize });
+
+    }
+
+    static getInterval({xScale, labelFontSize}) {
+
+        const spaceNeeded = labelFontSize + 6;      // 6 assumes a margin of 3 px on either side of the label (move this to viz defs?)
+        const bandwidth = xScale.bandwidth();
+
+        return Math.ceil( spaceNeeded / bandwidth );
+    }
+
+    static addModLabels({ svg, sortedModMasses, xScale, labelFontSize }) {
+
+        const interval = ModViewDataVizRenderer_MultiSearch.getInterval({labelFontSize, xScale});
+
+        svg.selectAll('.mod-label')
+            .data(sortedModMasses)
+            .enter()
+            .append('text')
+            .attr('class', 'mod-label')
+            .attr("text-anchor", "start")
+            .attr('font-size', labelFontSize + 'px')
+            .attr('font-family', 'sans-serif')
+            .attr("transform", (d,i) => 'translate(' + (xScale(sortedModMasses[i]) + (xScale.bandwidth() / 2) + (labelFontSize / 2)) + ', -10) rotate(-90)')
+            .text((d,i) => ( i % interval == 0 ? sortedModMasses[i] : '' ));
+    }
+
+    static addSearchLabels({ svg, projectSearchIds, yScale, searchDetailsBlockDataMgmtProcessing, maxSearchLabelLength, labelFontSize }) {
+
+        svg.selectAll('.project-label')
+            .data(projectSearchIds)
+            .enter()
+            .append('text')
+            .attr('class', 'project-label')
+            .attr('x', -10)
+            .attr('y', (d, i) => (yScale(projectSearchIds[i]) + (yScale.bandwidth() / 2) + (labelFontSize / 2)))
+            .attr("text-anchor", "end")
+            .attr('font-size', labelFontSize + 'px')
+            .attr('font-family', 'sans-serif')
+            .text((d,i) => (ModViewDataVizRenderer_MultiSearch.getTruncatedSearchNameForProjectSearchId({ projectSearchId:projectSearchIds[i], searchDetailsBlockDataMgmtProcessing, maxSearchLabelLength})));
+    }
+
+    static getTruncatedSearchNameForProjectSearchId({ projectSearchId, searchDetailsBlockDataMgmtProcessing, maxSearchLabelLength}) {
+
+        let searchName = ModViewDataVizRenderer_MultiSearch.getSearchNameForProjectSearchId({ projectSearchId, searchDetailsBlockDataMgmtProcessing });
+
+        if(searchName.length > maxSearchLabelLength) {
+            searchName = searchName.substring(0, maxSearchLabelLength - 4) + '...';
+        }
+
+        return searchName;
+
+    }
+
+    static getSearchNameForProjectSearchId({ projectSearchId, searchDetailsBlockDataMgmtProcessing}) {
+        const maxLength = 30;
+        const searchName = searchDetailsBlockDataMgmtProcessing._dataPageStateManager_DataFrom_Server._pageState.searchNames[projectSearchId].name;
+
+        return searchName;
+    }
+
+    static getWidth({sortedModMasses, widthDefs}) {
+
+        let width = widthDefs.default;
+
+        // adjust width as necessary
+        if(width < sortedModMasses.length * widthDefs.min) {
+            width = sortedModMasses.length * widthDefs.min;
+        } else if(width > sortedModMasses.length * widthDefs.max) {
+            width = sortedModMasses.length * widthDefs.max;
+        }
+
+        return width;
+    }
+
+    static getHeight({projectSearchIds, heightDefs}) {
+
+        let height = heightDefs.default;
+
+        // adjust width as necessary
+        if(height < projectSearchIds.length * heightDefs.min) {
+            height = projectSearchIds.length * heightDefs.min;
+        } else if(height > projectSearchIds.length * heightDefs.max) {
+            height = projectSearchIds.length * heightDefs.max;
+        }
+
+        return height;
+    }
+
+    static addColoredRectangles({ svg, modMatrix, xScale, yScale, colorScale, sortedModMasses, projectSearchIds }) {
 
         // Create a group for each column in the data matrix and translate the group horizontally
         const svgColGroups = svg.selectAll('.search-col-group')
@@ -52,7 +161,7 @@ export class ModViewDataVizRenderer_MultiSearch {
             .append('g')
             .attr('class', 'search-col-group')
             .attr('transform', (d, i) => ( 'translate(' + xScale(sortedModMasses[i]) + ',0)' )
-         );
+            );
 
         svgColGroups.selectAll('rect')
             .data(function(d) { return d; })
@@ -62,8 +171,11 @@ export class ModViewDataVizRenderer_MultiSearch {
             .attr('width', xScale.bandwidth())
             .attr('height', yScale.bandwidth())
             .attr('stroke', 'none')
-            .attr('fill', (d, i) => (colorScale(d)));
+            .attr('fill', (d) => (colorScale(d)));
 
+    }
+
+    static addSeparatorLines({ svg, projectSearchIds, yScale, width, height }) {
 
         svg.selectAll('.separator-line')
             .data(projectSearchIds)
@@ -146,7 +258,7 @@ export class ModViewDataVizRenderer_MultiSearch {
     static getModMatrix({modMap, projectSearchIds}) {
         let modMatrix = Array( Object.keys(modMap).length );
 
-        const sortedModMasses = Object.keys(modMap).sort();
+        const sortedModMasses = Object.keys(modMap).sort( (a,b) => ( parseInt(a) - parseInt(b)));
 
         let i = 0;
         for(const modMass of sortedModMasses) {
