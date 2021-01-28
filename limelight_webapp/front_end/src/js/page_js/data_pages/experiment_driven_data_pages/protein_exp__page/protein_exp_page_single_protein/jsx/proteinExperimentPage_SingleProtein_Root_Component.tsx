0cdd980e74d3f721b800a395fc2efe603e1a990a
danjasuw <djaschob@uw.edu>
2021-01-27 16:37:53 -0800

    Experiment Single Protein: Fix Header format for CSS changes
    Single Protein changed and changed CSS so Experiment Single Protein
    needed updates.

--- limelight_webapp/front_end/src/js/page_js/data_pages/experiment_driven_data_pages/protein_exp__page/protein_exp_page_single_protein/jsx/proteinExperimentPage_SingleProtein_Root_Component.tsx
+++ limelight_webapp/front_end/src/js/page_js/data_pages/experiment_driven_data_pages/protein_exp__page/protein_exp_page_single_protein/jsx/proteinExperimentPage_SingleProtein_Root_Component.tsx
@@ -19,7 +19,6 @@ import React from 'react'
 import { reportWebErrorToServer } from 'page_js/reportWebErrorToServer';
 import { ProteinExperimentPage_SingleProtein_MainContent_Component, ProteinExperimentPage_SingleProtein_MainContent_Component_Props_Prop } from './proteinExperimentPage_SingleProtein_MainContent_Component'
 import {ProteinExperimentPage_SingleProtein_ProteinNameDescription_Component} from "page_js/data_pages/experiment_driven_data_pages/protein_exp__page/protein_exp_page_single_protein/jsx/proteinExperimentPage_SingleProtein_ProteinNameDescription_Component";
-import {ProteinPage_Display_SingleProtein_ProteinNameDescription_Component} from "page_js/data_pages/project_search_ids_driven_pages/protein_page/protein_page_single_protein_common/proteinPage_Display_SingleProtein_ProteinNameDescription_Component";
 import {Spinner_Limelight_Component} from "page_js/common_all_pages/spinner_ReactComponent_Limelight";
 
 
@@ -74,6 +73,7 @@ export class ProteinExperimentPage_SingleProtein_Root_Component extends React.Co
     private _setWidth__view_single_protein_inner_overlay_div_BindThis = this._setWidth__view_single_protein_inner_overlay_div.bind(this);
 
     private _view_single_protein_inner_overlay_div_Ref : React.RefObject<HTMLDivElement>; //  React.createRef()  for container <div> id: view_single_protein_inner_overlay_div
+    private view_single_protein_overlay_header_inner_container_Ref : React.RefObject<HTMLDivElement>; //  React.createRef()
 
     private _view_single_protein_inner_overlay_div_Width : number = undefined;
 
@@ -91,6 +91,7 @@ export class ProteinExperimentPage_SingleProtein_Root_Component extends React.Co
         super(props);
 
         this._view_single_protein_inner_overlay_div_Ref = React.createRef<HTMLDivElement>();
+        this.view_single_protein_overlay_header_inner_container_Ref = React.createRef<HTMLDivElement>();
         this._view_single_protein_overlay_body_Ref = React.createRef<HTMLDivElement>();
 
         this.state = { 
@@ -306,6 +307,23 @@ export class ProteinExperimentPage_SingleProtein_Root_Component extends React.Co
             )
         }
 
+        let proteinName_Display_Header = null;
+
+        if ( this.props.proteinNames ) {
+            proteinName_Display_Header = this.props.proteinNames;
+        } else {
+            if (this.state.proteinExperimentPage_SingleProtein_MainContent_Component_Props_Prop) {
+                if ( this.state.proteinExperimentPage_SingleProtein_MainContent_Component_Props_Prop.proteinNames) {
+                    proteinName_Display_Header = this.state.proteinExperimentPage_SingleProtein_MainContent_Component_Props_Prop.proteinNames;
+                }
+            }
+        }
+
+        //  This CSS calculation sets the width the lesser of 100% of parent element or view port width (100vw) minus 10 pixels
+        //       (subtract 10 pixels since 100vw doesn't subtract for scrollbar if present)
+        //       (Set 'width:' and 'maxWidth:' to this string)
+        const view_single_protein_overlay_header_Style_width_maxWidth = "min( 100%, calc( 100vw - 10px ))";
+
         return (
             <div >
                 <div id="single_protein_overlay_background" className="single-protein-modal-dialog-overlay-background" >
@@ -317,12 +335,34 @@ export class ProteinExperimentPage_SingleProtein_Root_Component extends React.Co
 
                     <div id="view_single_protein_inner_overlay_div"  className=" view-single-protein-overlay-div " ref={ this._view_single_protein_inner_overlay_div_Ref }>
 
-                        <div id="view_single_protein_overlay_header" className="view-single-protein-overlay-header" style={ { width: "100%" } } >
+                        <div id="view_single_protein_overlay_header" className="view-single-protein-overlay-header" >
 
-                            <h1 id="view_single_protein_overlay_X_for_exit_overlay" className="view-single-protein-overlay-X-for-exit-overlay" onClick={ closeOverlayClickHandler }
-                            >X</h1>
-                                                
-                            <h1 id="view_single_protein_overlay_header_text" className="view-single-protein-overlay-header-text" >Protein Data</h1>
+                            <div ref={ this.view_single_protein_overlay_header_inner_container_Ref }
+                                 style={  { width: view_single_protein_overlay_header_Style_width_maxWidth,  maxWidth: view_single_protein_overlay_header_Style_width_maxWidth } }
+                            >
+
+                                <div  style={ { display: "grid", gridTemplateColumns: "min-content auto" } }>
+                                    {/* Next elements in the header are in a CSS Grid */}
+
+                                    <h1 className="view-single-protein-overlay-X-for-exit-overlay" onClick={ closeOverlayClickHandler }
+                                        title="Close"
+                                    >X</h1>
+
+                                    <h1 className="view-single-protein-overlay-header-text"
+                                        title={ proteinName_Display_Header }
+                                    >
+                                        <span>Protein</span>
+                                        { proteinName_Display_Header ? (
+                                            <span>: </span>
+                                        ) : null }
+                                        { proteinName_Display_Header }
+                                    </h1>
+                                </div>
+                            </div>
+                            {/*<h1 id="view_single_protein_overlay_X_for_exit_overlay" className="view-single-protein-overlay-X-for-exit-overlay" onClick={ closeOverlayClickHandler }*/}
+                            {/*>X</h1>*/}
+                            {/*                    */}
+                            {/*<h1 id="view_single_protein_overlay_header_text" className="view-single-protein-overlay-header-text" >Protein Data</h1>*/}
                         </div>
                         <div id="view_single_protein_overlay_body" className="view-single-protein-overlay-body" ref={ this._view_single_protein_overlay_body_Ref } >
 
