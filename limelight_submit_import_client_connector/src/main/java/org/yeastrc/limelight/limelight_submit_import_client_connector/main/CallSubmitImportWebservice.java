7dc89f84d018dd72281e01da13e0309672d17918
danj <djaschob@uw.edu>
2021-12-17 08:20:15 -0800

    Submitter Program (Client Connector): remove test code forcing bad URL

--- limelight_submit_import_client_connector/src/main/java/org/yeastrc/limelight/limelight_submit_import_client_connector/main/CallSubmitImportWebservice.java
+++ limelight_submit_import_client_connector/src/main/java/org/yeastrc/limelight/limelight_submit_import_client_connector/main/CallSubmitImportWebservice.java
@@ -241,7 +241,7 @@ public class CallSubmitImportWebservice {
 		Map<String,String> headersToSend = new HashMap<>();
 		headersToSend.put( SUBMIT_IMPORT_UPLOAD_FILE_HEADER_PARAMETER_PARAMS_PGM_XML, headerString );
 		
-		String webserviceURL = webappServerBaseURL + SUBMIT_IMPORT_UPLOAD_FILE_WEBSERVICE_SUB_URL + "__FAKE_EXTRA";
+		String webserviceURL = webappServerBaseURL + SUBMIT_IMPORT_UPLOAD_FILE_WEBSERVICE_SUB_URL;
 				
 		Object webserviceResponseAsObject = 
 				callActualWebserviceOnServerSendByteArrayOrFileAsStreamReturnObject(
