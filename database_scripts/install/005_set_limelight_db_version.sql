91b33036a7bf028d4d262799d827e28f47ad8d88
Dan Jaschob <djaschob@uw.edu>
2025-07-02 15:11:24 -0700

    DB: Update install script for DB version to 7
    Update to match code change to 7 a few commits ago:
    
    commit 611839ffe41ba72c422d3bd2ecff7afef90d8197
    DB V 7 Start:

--- database_scripts/install/005_set_limelight_db_version.sql
+++ database_scripts/install/005_set_limelight_db_version.sql
@@ -1,15 +1,15 @@
 
 --   Set Limelight DB Version for initial install, BOTH records MUST have SAME value.
 
---      Current version is:  6
+--      Current version is:  7
 
 USE limelight ;
 
 
 INSERT INTO aa_limelight_database_version_tbl
 (row_label, limelight_database_version_number)
-VALUES ('DB Version In Progress', 6 );
+VALUES ('DB Version In Progress', 7 );
 
 
 INSERT INTO aa_limelight_database_version_tbl (row_label, limelight_database_version_number)
-VALUES ('DB Version Current', 6 );
+VALUES ('DB Version Current', 7 );
