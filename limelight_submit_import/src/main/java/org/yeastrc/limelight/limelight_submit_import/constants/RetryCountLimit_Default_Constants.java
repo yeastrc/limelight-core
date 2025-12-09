df8a27cca038e8b12bb3899e3a42013137650090
Dan Jaschob <djaschob@uw.edu>
2025-12-09 13:33:43 -0800

    Submit Import Pgm: Update Default Retry and Program Version Number
    Change to:
    RETRY_COUNT_LIMIT_DEFAULT = 6
    SUBMIT_PROGRAM__CURRENT__VERSION_NUMBER = 13;

--- limelight_submit_import/src/main/java/org/yeastrc/limelight/limelight_submit_import/constants/RetryCountLimit_Default_Constants.java
+++ limelight_submit_import/src/main/java/org/yeastrc/limelight/limelight_submit_import/constants/RetryCountLimit_Default_Constants.java
@@ -2,5 +2,5 @@ package org.yeastrc.limelight.limelight_submit_import.constants;
 
 public class RetryCountLimit_Default_Constants {
 
-	public static final int RETRY_COUNT_LIMIT_DEFAULT = 1;
+	public static final int RETRY_COUNT_LIMIT_DEFAULT = 6;  // Retry delay is 5 seconds so total 30 seconds
 }
